aa87503285963cfba19be489ac3deb0af319e116
Michael Hodge <michael.hodge@ons.gov.uk>
2021-04-27 15:38:40 +0100

    init commit

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import tweepy
+from datetime import datetime, timedelta
+
+# options
+days_to_delete_after = 31
+
+auth and api
+consumer_key = os.environ(TWITTER_API_KEY)
+consumer_secret = os.environ(TWITTER_API_SECRET_KEY)
+access_token = os.environ(ACCESS_TOKEN)
+access_token_secret =  os.environ(ACCESS_TOKEN_SECRET)
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# set cutoff date, use utc to match twitter
+cutoff_date = datetime.utcnow() - timedelta(days=days_to_delete_after)
+
+timeline = tweepy.Cursor(api.user_timeline).items()
+deletion_count = 0
+
+for tweet in timeline:
+	if tweet.created_at < cutoff_date:
+		api.destroy_status(tweet.id)
+		deletion_count += 1
+
+print(f"{deletion_count} tweets happened before {cutoff_date.date()}, these have now been deleted")
+
+favorites = tweepy.Cursor(api.favorites).items()
+unlike_count = 0
+
+for tweet in favorites:
+	if tweet.created_at < cutoff_date:
+		api.destroy_favorite(tweet.id)
+		unlike_count += 1
+
+print(f"{unlike_count} likes happened before {cutoff_date.date()}, these have now been unliked")
